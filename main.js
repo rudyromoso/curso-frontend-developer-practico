d5ebf7aadf57295f2f3425efdd1c4720a07d221e
Rudy Romero <reromero@sprc.com.co>
2023-03-13 19:41:05 -0500

    Codigo optimizado

--- main.js
+++ main.js
@@ -1,15 +1,59 @@
+
+/*
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carritoCompra = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', bloqDesblockDesktopMenu);
-//shoppingCart.addEventListener('click', bloqDesblockDesktopMenu);
+shoppingCartIcon.addEventListener('click', bloqDesblockCarrito);
+iconMenu.addEventListener('click', bloqDesbloqMobileMenu);
+//iconMenu.addEventListener('click', bloqDesbloqMobileMenu);
+
 
 
 
 
 function bloqDesblockDesktopMenu(){    
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive');// - toggle(): alterna entre agregar o eliminar una clase del elemento 
+    //desktopMenu.classList.remove('inactive');//- remove(): elimina una clase del elemento 
 }
 
 
+function bloqDesbloqMobileMenu(){
+    
+    mobileMenu.classList.toggle('inactive');
+    carritoCompra.classList.add('inactive');
+    
+}
+
+function bloqDesblockCarrito(){
+    
+    carritoCompra.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    //iconMenu.style.visibility = "hidden";
+}
+
+
+*/
+
+
+// Optimizado
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carritoCompra = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', () => desktopMenu.classList.toggle('inactive')); // se optimizo la función bloqDesblockDesktopMenu
+shoppingCartIcon.addEventListener('click', () => {
+    carritoCompra.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+}); // se optimizo la función bloqDesblockCarrito 
+iconMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
+    carritoCompra.classList.add('inactive');
+}); // se optimizo la función bloqDesbloqMobileMenu
