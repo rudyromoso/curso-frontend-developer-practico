b660815af18aaec43da0b66dd9faddbb9d9a22db
Rudy Romero <reromero@sprc.com.co>
2023-03-15 16:46:16 -0500

    Mostrando los datos del producto seleccionado

--- main.js
+++ main.js
@@ -50,6 +50,8 @@ const carritoCompra = document.querySelector('#ShoppingCartContainer');
 const listaDeProductos = document.querySelector('.cards-container');
 const productDetailContainer = document.querySelector('#ProductDetailContainer');
 const productDetailClose = document.querySelector('.product-detail-close');
+const productSelectDetailValue = document.querySelector('.product-info');
+const productSelectImage = document.querySelector('.imageProduct')
 
 
 menuEmail.addEventListener('click', () => {
@@ -63,7 +65,7 @@ shoppingCartIcon.addEventListener('click', () => {
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
     productDetailContainer.classList.add('inactive');
-   
+
 
 });
 iconMenu.addEventListener('click', () => {
@@ -77,54 +79,85 @@ productDetailClose.addEventListener('click', () => {
 });
 
 
+function visualizarDetalleProduct(valor, nombre, descrip, imagen) {
+    console.log(imagen);
+    productSelectImage.src = imagen;
+    productSelectDetailValue.innerHTML = `<p>COP$ ${valor.toLocaleString('es-CO')}</p> <p>${nombre}</p> <p>${descrip}</p>
+    <button class="primary-button add-to-cart-button"><img src="./icons/bt_add_to_cart.svg" alt="add to cart">Add to cart</button>`;
+
+
+
+}
+
 const productos = [];
 
 productos.push({
-    nombre: "Bicicleta",
-    descripcion: "Bicicleta de montaña",
-    valor: 450000,
+    nombre: "Bicicleta Optimus 29\"",
+    descripcion: "Aquila, la bicicleta que ha llegado como un águila a coronarse dentro de las bicicletas de MTB como una de las más majestuosas," +
+        " poderosas y veloces. Su cuadro de aluminio la hace más liviana y capaz de conquistar distintos terrenos sin problemas." +
+        "Con 8 velocidades y un biplato, esta bicicleta de montaña te llevará a cualquier zona que desees sin titubear, además, cuenta con un tenedor " +
+        "de bloqueo mecánico que hace más ligero el peso de la bicicleta y te prepara para las subidas más difíciles de la montaña.",
+    valor: 4525300,
+    imagen: 'https://www.costco.com.mx/medias/sys_master/products/hd5/h89/79563089313822.webp',
 },
     {
-        nombre: "Computador",
-        descripcion: "Computador MAC",
-        valor: 3200000,
+        nombre: "MacBook PRO 16\"",
+        descripcion: "El modelo de 16 pulgadas tiene la batería de mayor duración en un Mac. Y los dos modelos mantienen su rendimiento" +
+            "al máximo aunque no estén conectados a la corriente. Parece magia, pero es la eficiencia de los chips de Apple. Así, podrás darlo" +
+            " todo cuando te llegue la inspiración o el deber te llame.",
+        valor: 13459000,
+        imagen: 'https://www.apple.com/la/macbook-pro/images/overview/hero_13__d1tfa5zby7e6_large.jpg',
     },
     {
-        nombre: "Monitor",
-        descripcion: "Monitor Dell 27\"",
+        nombre: "Monitor Dell 23,8\"",
+        descripcion: "Una mejor vista desde cualquier lugar, Mantenga la productividad, sin importar dónde trabaje. Reduzca la dañina luz azul con este elegante monitor FHD de 23,8 pulgadas que cuenta con tecnología ComfortView Plus.",
         valor: 1800000,
+        imagen: 'https://snpi.dell.com/snp/images/products/large/es-co~210-BBCC/210-BBCC.jpg',
     });
 
 
-function agregarProductosALaLista(productos) {
 
+function agregarProductosALaLista(productos) {
+    let indice = 0;
     for (pr of productos) {
 
         const productCard = document.createElement('div');
         productCard.className = 'product-card';
 
         const img = document.createElement('img');
-        img.src = 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+        img.src = pr.imagen;
         img.alt = '';
+        // img.src = 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
 
-        img.addEventListener('click', () => {
+
+        img.addEventListener('click', (e) => {
             productDetailContainer.classList.remove('inactive');
             carritoCompra.classList.add('inactive');
             desktopMenu.classList.add('inactive');
+            console.log(`Se hizo click en el  producto: ${indicador.textContent}`);
+            visualizarDetalleProduct(productos[indicador.textContent].valor, productos[indicador.textContent].nombre, productos[indicador.textContent].descripcion, productos[indicador.textContent].imagen);
         });
 
         const productInfo = document.createElement('div');
         productInfo.className = 'product-info';
 
+
+
         const productPrice = document.createElement('p');
-        productPrice.innerHTML = "$ " + pr.valor.toLocaleString('es-CO');
+        productPrice.innerHTML = "COP$ " + pr.valor.toLocaleString('es-CO');
 
         const productName = document.createElement('p');
-        productName.innerHTML = pr.descripcion;
+        productName.innerHTML = pr.nombre;
+
+        const indicador = document.createElement('p');
+        indicador.innerHTML = indice;
+        indice++;
 
         const buttonDiv = document.createElement('div');
         buttonDiv.appendChild(productPrice);
         buttonDiv.appendChild(productName);
+        buttonDiv.appendChild(indicador).style.display = "none";
+
 
         const buttonImg = document.createElement('img');
         buttonImg.src = './icons/bt_add_to_cart.svg';
