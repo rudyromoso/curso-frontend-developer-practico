ca784fafb3f8a634394a897831a6818838c5dd2b
Rudy Romero <reromero@sprc.com.co>
2023-03-15 10:21:13 -0500

    integrando el detalle del producto

--- main.js
+++ main.js
@@ -46,26 +46,32 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const carritoCompra = document.querySelector('.product-detail');
+const carritoCompra = document.querySelector('#ShoppingCartContainer');
 const listaDeProductos = document.querySelector('.cards-container');
+//const linkAll = document.querySelector('.linkAll');
 
 
 menuEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
     carritoCompra.classList.add('inactive');
-}); // se optimizo la función bloqDesblockDesktopMenu
+}); 
 
 shoppingCartIcon.addEventListener('click', () => {
     carritoCompra.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
-}); // se optimizo la función bloqDesblockCarrito 
+}); 
 iconMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('inactive');
     carritoCompra.classList.add('inactive');
-}); // se optimizo la función bloqDesbloqMobileMenu
+}); 
 
-
+// se trata de asociar la vista de la lista de producto al link ALL.
+/*
+linkAll.addEventListener('click', () => {
+    listaDeProductos.classList.toggle('inactive');  
+});
+*/
 
 const productos = [];
 
@@ -113,7 +119,7 @@ function agregarProductosALaLista(productos) {
         const buttonImg = document.createElement('img');
         buttonImg.src = './icons/bt_add_to_cart.svg';
         buttonImg.alt = '';
-        
+
         const buttonFigure = document.createElement('figure');
         buttonFigure.appendChild(buttonImg);
 
